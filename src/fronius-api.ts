ba16d0f4394050319dd33ef84d665bfa64b1578a
Long Zheng <long.zheng@gmail.com>
2021-05-25 20:52:52 +1000

    Add types to Fronius API

--- src/fronius-api.ts
+++ src/fronius-api.ts
@@ -17,7 +17,7 @@ export class FroniusApi {
 
   public getInverterData = async () => {
     try {
-      return await this.http.get(
+      return await this.http.get<FroniusRealtimeData>(
         'http://' +
           this.inverterIp +
           '/solar_api/v1/GetPowerFlowRealtimeData.fcgi',
@@ -29,4 +29,47 @@ export class FroniusApi {
       return null;
     }
   };
+}
+
+export interface FroniusRealtimeData {
+  Body: Body;
+  Head: Head;
+}
+export interface Body {
+  Data: Data;
+}
+export interface Data {
+  Inverters: Record<string, Inverter>;
+  Site: Site;
+  Version: string;
+}
+export interface Inverter {
+  DT: number;
+  E_Day: number;
+  E_Total: number;
+  E_Year: number;
+  P: number;
+}
+export interface Site {
+  E_Day: number;
+  E_Total: number;
+  E_Year: number;
+  Meter_Location: string;
+  Mode: string;
+  P_Akku: number | null;
+  P_Grid: number;
+  P_Load: number;
+  P_PV: number | null;
+  rel_Autonomy: number;
+  rel_SelfConsumption: number | null;
+}
+export interface Head {
+  RequestArguments: Record<string, unknown>;
+  Status: Status;
+  Timestamp: string;
+}
+export interface Status {
+  Code: number;
+  Reason: string;
+  UserMessage: string;
 }
