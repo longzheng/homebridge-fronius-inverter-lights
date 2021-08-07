f45e52365933ab98e392d97696e8aabdae7317d8
Long Zheng <long.zheng@gmail.com>
2021-08-07 16:02:51 +1000

    Optimize polling
    Don't hook into GET event, only poll and update
    Reuse one Fronius API request between import/export accessories

--- src/fronius-api.ts
+++ src/fronius-api.ts
@@ -5,6 +5,7 @@ export class FroniusApi {
   private readonly http: AxiosInstance;
   private readonly inverterIp: string;
   private readonly log: Logging;
+  private request: Promise<any> | undefined; // cache the current request to prevent concurrent requests
 
   constructor(inverterIp: string, log: Logging) {
     this.inverterIp = inverterIp;
@@ -16,15 +17,38 @@ export class FroniusApi {
   }
 
   public getInverterData = async () => {
-    try {
-      return await this.http.get(
-        'http://' +
-          this.inverterIp +
-          '/solar_api/v1/GetPowerFlowRealtimeData.fcgi',
-      );
-    } catch (error) {
-      this.log.error(error);
-      return null;
+    // if request is already in operation, return the previous request
+    if (this.request) {
+      return this.request;
     }
+
+    this.request = new Promise((resolve) => {
+      const url = `http://${this.inverterIp}/solar_api/v1/GetPowerFlowRealtimeData.fcgi`;
+
+      this.log.debug(`Getting inverter data: ${url}`);
+
+      this.http
+        .get(url)
+        .then((response) => {
+          // clear existing request
+          this.request = undefined;
+
+          if (response.status === 200) {
+            return resolve(response.data.Body.Data.Site);
+          } else {
+            this.log.error(`Received invalid status code: ${response.status}`);
+
+            return resolve(null);
+          }
+        })
+        .catch((error) => {
+          this.log.error(error.message);
+
+          this.request = undefined;
+          return resolve(null);
+        });
+    });
+
+    return this.request;
   };
 }
